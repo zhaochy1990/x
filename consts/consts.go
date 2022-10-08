5ed5ec551e787c6c739db1bfb15eb477e99a7a49
zhaochy <zhaochy_2015@hotmail.com>
2022-10-08 19:54:55 +0800

    feat: add runtime
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

---
+++ consts/consts.go
@@ -0,0 +1,10 @@
+package consts
+
+var (
+	//ConfigPath environment variable name
+	ConfigPath = "CONFIG_PATH"
+	//ConfigFileName environment variable name
+	ConfigFileName = "CONFIG_FILE_NAME"
+	//DefaultConfigPath is the default place where we are looking for configuration
+	DefaultConfigPath = "/etc/viper.yml"
+)
